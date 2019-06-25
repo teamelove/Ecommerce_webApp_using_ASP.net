9a3bf5ae54f6d8e1e97d7db8e047009ac3890737
teamelove <47137947+teamelove@users.noreply.github.com>
2019-06-25 15:46:56 +0900

    Add project files.

---
+++ Ecommerce/Controllers/AdminController.cs
@@ -0,0 +1,50 @@
+﻿using Ecommerce.DAL;
+using Ecommerce.Models;
+using Ecommerce.Repository;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+        public ActionResult Categories()
+        {
+            List<Tbl_Category> allcategories = _unitOfWork.GetRepositoryInstance<Tbl_Category>().GetAllRecordsIQueryable().Where(i => i.IsDelete == false).ToList();
+            return View(allcategories);
+        }
+        public ActionResult AddCategory()
+        {
+            return UpdateCategory(0);
+        }
+        public ActionResult UpdateCategory(int categoryId)
+        {
+            CategoryDetail cd;
+            
+           if(categoryId != null)
+            {
+                cd = JsonConvert.DeserializeObject<CategoryDetail>(JsonConvert.SerializeObject(_unitOfWork.GetRepositoryInstance<Tbl_Category>().GetFirstorDefault(categoryId)));
+            }
+            else
+            {
+                cd = new CategoryDetail();
+            }
+            return View("UpdateCategory", cd); 
+        }
+        public ActionResult Product()
+        {
+            return View(_unitOfWork.GetRepositoryInstance<Tbl_Product>().GetProduct());
+        }
+    }
+}
